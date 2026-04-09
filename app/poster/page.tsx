1640d709937d5c5e716bdb1a0525df09b219ca1c
Dhillu <mddhillu12@gmail.com>
2026-04-09 17:49:09 +0300

    Final fix for poster paths

--- app/poster/page.tsx
+++ app/poster/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+// We are putting the logic directly here to avoid "Module Not Found" errors
 import PosterBuilder from "../components/PosterBuilder";
 
 export default function PosterPage() {
